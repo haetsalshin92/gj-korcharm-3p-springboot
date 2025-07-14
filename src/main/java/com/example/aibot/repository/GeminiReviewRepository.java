abf086dee8109930d9255362a6753e9b1f697dbd
haetsalshin92 <sinsin09022@gmail.com>
2025-07-14 10:40:48 +0900

    mongodb 저장하도록 수정2

--- src/main/java/com/example/aibot/repository/GeminiReviewRepository.java
+++ src/main/java/com/example/aibot/repository/GeminiReviewRepository.java
@@ -1,6 +1,8 @@
 package com.example.aibot.repository;
 import com.example.aibot.dto.GeminiReview;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GeminiReviewRepository extends MongoRepository<GeminiReview, String> {
 }
